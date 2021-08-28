8e2a7735d4321077d71a2baa5b974e1b599117a7
anzhiyu <2268025923@qq.com>
2021-08-28 11:18:50 +0800

    feat(main): anzhiyu add main

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+/*
+ * @Description:提交限制文件
+ * @Author: 安知鱼
+ * @Email: 2268025923@qq.com
+ * @Date: 2021-08-28 11:07:12
+ * @LastEditTime: 2021-08-28 11:10:33
+ * @LastEditors: 安知鱼
+ */
+
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+};
